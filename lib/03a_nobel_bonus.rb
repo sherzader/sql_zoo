45a7904d05d79140564e7d53568e473389cb110e
sheri <sheri.h.zada@gmail.com>
2015-11-09 15:30:35 -0800

    Start work on subqueries

--- lib/03a_nobel_bonus.rb
+++ lib/03a_nobel_bonus.rb
@@ -14,5 +14,16 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+  SELECT
+    yr
+  FROM
+    nobels
+  WHERE
+    yr
+    IN
+    (
+      SELECT 
+
+    )
   SQL
 end
